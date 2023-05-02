afe74ec9c88dba5fcc8a6ca415198f82d2d4d4ba
gastonloco <ferreyragaston351@gmail.com>
2023-05-02 14:43:44 -0300

    inicio 1-1

--- webEsp32/lib/SuperMon/SuperMon.h
+++ webEsp32/lib/SuperMon/SuperMon.h
@@ -65,152 +65,150 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
 
 <title>Web Page Update Demo</title>
 
-  <style>
-    table {
-      position: relative;
-      width:100%;
-      border-spacing: 0px;
+<style>
+  table {
+    position: relative;
+    width:100%;
+    border-spacing: 0px;
+  }
+  tr {
+    border: 1px solid white;
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 20px;
+  }
+  th {
+    height: 20px;
+    padding: 3px 15px;
+    background-color: #343a40;
+    color: #FFFFFF !important;
     }
-    tr {
-      border: 1px solid white;
-      font-family: "Verdana", "Arial", sans-serif;
-      font-size: 20px;
-    }
-    th {
-      height: 20px;
-      padding: 3px 15px;
-      background-color: #343a40;
-      color: #FFFFFF !important;
-      }
-    td {
-      height: 20px;
-       padding: 3px 15px;
-    }
-    .tabledata {
-      font-size: 24px;
-      position: relative;
-      padding-left: 5px;
-      padding-top: 5px;
-      height:   25px;
-      border-radius: 5px;
-      color: #FFFFFF;
-      line-height: 20px;
-      transition: all 200ms ease-in-out;
-      background-color: #00AA00;
-    }
-    .fanrpmslider {
-      width: 30%;
-      height: 55px;
-      outline: none;
-      height: 25px;
-    }
-    .bodytext {
-      font-family: "Verdana", "Arial", sans-serif;
-      font-size: 24px;
-      text-align: left;
-      font-weight: light;
-      border-radius: 5px;
-      display:inline;
-    }
-    .navbar {
-      width: 100%;
-      height: 50px;
-      margin: 0;
-      padding: 10px 0px;
-      background-color: #FFF;
-      color: #000000;
-      border-bottom: 5px solid #293578;
-    }
-    .fixed-top {
-      position: fixed;
-      top: 0;
-      right: 0;
-      left: 0;
-      z-index: 1030;
-    }
-    .navtitle {
-      float: left;
-      height: 50px;
-      font-family: "Verdana", "Arial", sans-serif;
-      font-size: 50px;
-      font-weight: bold;
-      line-height: 50px;
-      padding-left: 20px;
-    }
-.navheading {
-  position: fixed;
-  left: 60%;
-  height: 50px;
-  font-family: "Verdana", "Arial", sans-serif;
-  font-size: 20px;
-  font-weight: bold;
-  line-height: 20px;
-  padding-right: 20px;
-}
-.navdata {
-  justify-content: flex-end;
-  position: fixed;
-  left: 70%;
-  height: 50px;
-  font-family: "Verdana", "Arial", sans-serif;
-  font-size: 20px;
-  font-weight: bold;
-  line-height: 20px;
-  padding-right: 20px;
-}
-.category {
-  font-family: "Verdana", "Arial", sans-serif;
-  font-weight: bold;
-  font-size: 32px;
-  line-height: 50px;
-  padding: 20px 10px 0px 10px;
-  color: #000000;
-}
-.heading {
-  font-family: "Verdana", "Arial", sans-serif;
-  font-weight: normal;
-  font-size: 28px;
-  text-align: left;
-}
-
-.btn {
-  background-color: #444444;
-  border: none;
-  color: white;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-.foot {
-  font-family: "Verdana", "Arial", sans-serif;
-  font-size: 20px;
-  position: relative;
-  height:   30px;
-  text-align: center;   
-  color: #AAAAAA;
-  line-height: 20px;
-}
-.container {
-  max-width: 1800px;
-  margin: 0 auto;
-}
-table tr:first-child th:first-child {
-  border-top-left-radius: 5px;
-}
-table tr:first-child th:last-child {
-  border-top-right-radius: 5px;
-}
-table tr:last-child td:first-child {
-  border-bottom-left-radius: 5px;
-}
-table tr:last-child td:last-child {
-  border-bottom-right-radius: 5px;
-}
-    
+  td {
+    height: 20px;
+    padding: 3px 15px;
+  }
+  .tabledata {
+    font-size: 24px;
+    position: relative;
+    padding-left: 5px;
+    padding-top: 5px;
+    height:   25px;
+    border-radius: 5px;
+    color: #FFFFFF;
+    line-height: 20px;
+    transition: all 200ms ease-in-out;
+    background-color: #00AA00;
+  }
+  .fanrpmslider {
+    width: 30%;
+    height: 55px;
+    outline: none;
+    height: 25px;
+  }
+  .bodytext {
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 24px;
+    text-align: left;
+    font-weight: light;
+    border-radius: 5px;
+    display:inline;
+  }
+  .navbar {
+    width: 100%;
+    height: 50px;
+    margin: 0;
+    padding: 10px 0px;
+    background-color: #FFF;
+    color: #000000;
+    border-bottom: 5px solid #293578;
+  }
+  .fixed-top {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1030;
+  }
+  .navtitle {
+    float: left;
+    height: 50px;
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 50px;
+    font-weight: bold;
+    line-height: 50px;
+    padding-left: 20px;
+  }
+  .navheading {
+    position: fixed;
+    left: 60%;
+    height: 50px;
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 20px;
+    padding-right: 20px;
+  }
+  .navdata {
+    justify-content: flex-end;
+    position: fixed;
+    left: 70%;
+    height: 50px;
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 20px;
+    padding-right: 20px;
+  }
+  .category {
+    font-family: "Verdana", "Arial", sans-serif;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 50px;
+    padding: 20px 10px 0px 10px;
+    color: #000000;
+  }
+  .heading {
+    font-family: "Verdana", "Arial", sans-serif;
+    font-weight: normal;
+    font-size: 28px;
+    text-align: left;
+  }
+  .btn {
+    background-color: #444444;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+  .foot {
+    font-family: "Verdana", "Arial", sans-serif;
+    font-size: 20px;
+    position: relative;
+    height:   30px;
+    text-align: center;   
+    color: #AAAAAA;
+    line-height: 20px;
+  }
+  .container {
+    max-width: 1800px;
+    margin: 0 auto;
+  }
+  table tr:first-child th:first-child {
+    border-top-left-radius: 5px;
+  }
+  table tr:first-child th:last-child {
+    border-top-right-radius: 5px;
+  }
+  table tr:last-child td:first-child {
+    border-bottom-left-radius: 5px;
+  }
+  table tr:last-child td:last-child {
+    border-bottom-right-radius: 5px;
+  }   
 </style>
 
 <body style="background-color: #efefef" onload="process()">
@@ -438,8 +436,7 @@ function response(){
       }
       else{
         document.getElementById("btn0").innerHTML="Turn OFF";
-      }
-         
+      }      
       xmldoc = xmlResponse.getElementsByTagName("SWITCH");
       message = xmldoc[0].firstChild.nodeValue;
       document.getElementById("switch").style.backgroundColor="rgb(200,200,200)";
@@ -454,7 +451,7 @@ function response(){
         document.getElementById("btn1").innerHTML="Turn OFF";
         document.getElementById("switch").style.color="#00AA00";
       }
-     }
+    }
   
     // general processing code for the web page to ask for an XML steam
     // this is actually why you need to keep sending data to the page to 
@@ -462,8 +459,7 @@ function response(){
     // server.on("/xml", SendXML);
     // otherwise the page will not request XML from the ESP, and updates will not happen
     function process(){
-     
-     if(xmlHttp.readyState==0 || xmlHttp.readyState==4) {
+    if(xmlHttp.readyState==0 || xmlHttp.readyState==4) {
         xmlHttp.open("PUT","xml",true);
         xmlHttp.onreadystatechange=response;
         xmlHttp.send(null);
@@ -472,12 +468,8 @@ function response(){
         // a longer timeout
         setTimeout("process()",100);
     }
-  
-  
   </script>
-
 </html>
-
 
 
 )=====";
